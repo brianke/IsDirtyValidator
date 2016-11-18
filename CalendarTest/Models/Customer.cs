ef70741caa4eb04dd2ff6dff9b4ef1d3dcc6ef9f
brianke <brianke@gmail.com>
2016-11-17 20:09:53 -0500

    Add files via upload

---
+++ CalendarTest/Models/Customer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using SimpleMvvmToolkit;
+using System.Windows.Controls;
+
+namespace CalendarTest
+{
+    public class Customer : ModelBase<Customer>
+    {
+        private int customerId;
+        public int CustomerId
+        {
+            get { return customerId; }
+            set
+            {
+                customerId = value;
+                NotifyPropertyChanged(m => m.CustomerId);
+            }
+        }
+
+        private string customerName;
+        public string CustomerName
+        {
+            get { return customerName; }
+            set
+            {
+                customerName = value;
+                NotifyPropertyChanged(m => m.CustomerName);
+            }
+        }
+
+        private string city;
+        public string City
+        {
+            get { return city; }
+            set
+            {
+                city = value;
+                NotifyPropertyChanged(m => m.City);
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Validate ASM Code
+    /// </summary>
+    public class IsAlphaOnlyValidator : ValidationRule
+    {
+        /// <summary>
+        /// Validate provided ASM database name
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="cultureInfo"></param>
+        /// <returns></returns>
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            return new ValidationResult(value.ToString().All(Char.IsLetter), "Value must be text only.");
+        }
+    }
+
+
+}
---
+++ CalendarTest/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,85 @@
+﻿// Toolkit namespace
+using SimpleMvvmToolkit;
+using System;
+
+// Toolkit extension methods
+
+namespace CalendarTest
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvmprop</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// </summary>
+    public class MainPageViewModel : ViewModelDetailBase<MainPageViewModel, Customer>
+    {
+        #region Initialization and Cleanup
+
+        // Default ctor
+        public MainPageViewModel() 
+        {
+            // Create new customer
+            Customer = new Customer()
+            {
+                CustomerName = "Brian",
+                City = "Powell",
+                CustomerId = 123
+            };
+
+            Model = Customer;
+            BeginEdit();
+        }
+        
+
+        #endregion
+
+        #region Notifications
+
+        // TODO: Add events to notify the view or obtain data from the view
+        public event EventHandler<NotificationEventArgs<Exception>> ErrorNotice;
+
+        #endregion
+
+        #region Properties
+
+        public Customer Customer
+        {
+            get { return _Customer; }
+             set
+             {
+                 _Customer = value;
+                 NotifyPropertyChanged(m => m.Customer);
+             }
+         }
+        private Customer _Customer;
+
+
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Completion Callbacks
+
+        // TODO: Optionally add callback methods for async calls to the service agent
+
+        #endregion
+
+        #region Helpers
+
+        // Helper method to notify View of an error
+        private void NotifyError(string message, Exception error)
+        {
+            // Notify view of an error
+            Notify(ErrorNotice, new NotificationEventArgs<Exception>(message, error));
+        }
+
+        #endregion
+    }
+
+
+
+}
